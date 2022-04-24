bd4cbcfe61427071d9ead49107c92ca23713a812
Matthew <willivms02@hotmail.com>
2022-04-24 17:18:54 -0400

    questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,31 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs')
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions =  () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Title',
+            message: "What is the title of your project?"
+        },
+        {
+            type: 'input',
+            name: 'Project Description',
+            message: "Describe your project"
+        },
+        {
+            type: 'input',
+            name: 'Table of Contents',
+            message: 'Provide the contents within your project'
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: "How do you install and run your project?"
+        },
+    ]);
+}    
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
